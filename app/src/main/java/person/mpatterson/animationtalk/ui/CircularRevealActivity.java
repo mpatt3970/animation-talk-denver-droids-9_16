48182aae31a9164ee78a9fb562252e22def8ffba
Brandon Davis <brandon.davis@possible.com>
2016-09-20 17:13:19 -0600

    get circular reveal activity working sort of

--- app/src/main/java/person/mpatterson/animationtalk/ui/CircularRevealActivity.java
+++ app/src/main/java/person/mpatterson/animationtalk/ui/CircularRevealActivity.java
@@ -1,10 +1,11 @@
 package person.mpatterson.animationtalk.ui;
 
 import android.animation.Animator;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 
-import java.util.ArrayDeque;
 import java.util.Queue;
 
 import person.mpatterson.animationtalk.R;
@@ -12,6 +13,11 @@ import person.mpatterson.animationtalk.helper.BaseActivity;
 import person.mpatterson.animationtalk.helper.Phase;
 
 public class CircularRevealActivity extends BaseActivity {
+
+    private boolean pause;
+    private boolean exit;
+    private boolean firstAnimComplete;
+
     @Override
     protected int getLayoutResourceId() {
         return R.layout.activity_circular_reveal;
@@ -19,29 +25,49 @@ public class CircularRevealActivity extends BaseActivity {
 
     @Override
     protected Queue<Phase> buildPhases() {
-        View correctReveal = findViewById(R.id.correct_reveal);
-        int correctX = correctReveal.getWidth() / 2;
-        int correctY = correctReveal.getHeight() / 2;
-        float correctRadius = (float) Math.hypot(correctX, correctY);
-        Animator correctAnimator = ViewAnimationUtils.createCircularReveal(correctReveal, correctX, correctY, 0, correctRadius);
-        //correctReveal.setVisibility(View.VISIBLE);
-
-        View incorrectReveal = findViewById(R.id.incorrect_reveal);
-        int incorrectX = incorrectReveal.getWidth() / 2;
-        int incorrectY = incorrectReveal.getHeight() / 2;
-        float incorrectRadius = (float) Math.hypot(incorrectX, incorrectY);
-        Animator incorrectAnimator = ViewAnimationUtils.createCircularReveal(incorrectReveal, incorrectX, incorrectY, 0, incorrectRadius);
-        //correctReveal.setVisibility(View.VISIBLE);
-
-        Queue<Phase> phaseQueue = new ArrayDeque<>();
-        phaseQueue.add(new Phase(correctAnimator, false, this));
-        phaseQueue.add(new Phase(incorrectAnimator, true, this));
-
-        return phaseQueue;
+        return null;
     }
 
     @Override
     protected Class getNextActivityClass() {
-        return QuestionsActivity.class;
+        if (exit) {
+            return QuestionsActivity.class;
+        } else if (pause) {
+            exit = true;
+        } else {
+            if (!firstAnimComplete) {
+                View correctReveal = findViewById(R.id.correct_reveal);
+
+                Display display = getWindowManager().getDefaultDisplay();
+                Point size = new Point();
+                display.getSize(size);
+
+                int correctX = size.x / 2;
+                int correctY = size.y / 2;
+                float correctRadius = (float) Math.hypot(correctX, correctY);
+
+                Animator correctAnimator = ViewAnimationUtils.createCircularReveal(correctReveal, correctX, correctY, 0, correctRadius);
+                correctReveal.setVisibility(View.VISIBLE);
+                correctAnimator.start();
+                firstAnimComplete = true;
+            } else {
+                pause = true;
+
+                View incorrectReveal = findViewById(R.id.incorrect_reveal);
+
+                Display display = getWindowManager().getDefaultDisplay();
+                Point size = new Point();
+                display.getSize(size);
+
+                int incorrectX = size.x / 2;
+                int incorrectY = size.y / 2;
+
+                float incorrectRadius = size.x / 2;
+                Animator incorrectAnimator = ViewAnimationUtils.createCircularReveal(incorrectReveal, incorrectX, incorrectY, 0, incorrectRadius);
+                incorrectReveal.setVisibility(View.VISIBLE);
+                incorrectAnimator.start();
+            }
+        }
+        return null;
     }
 }
